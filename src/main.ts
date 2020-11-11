294e2ecd42cea5e805350128e82e02d2fe73dbe1
Timothy McLane <timothyamclane@gmail.com>
2020-11-10 18:44:54 -0600

    Removing example code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
 import fs from 'fs'
 import request from 'request-promise-native'
 
@@ -25,14 +24,6 @@ async function run(): Promise<void> {
         version: config.version,
       },
     })
-
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
