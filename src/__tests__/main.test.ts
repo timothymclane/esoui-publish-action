f413f6aaebffaf6bd4b504981278539d21f9f405
Timothy McLane <timothyamclane@gmail.com>
2020-11-10 18:38:33 -0600

    Restoring parserOptions

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -1,7 +1,4 @@
-import {wait} from '../wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import { wait } from '../wait'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -12,16 +9,6 @@ test('wait 500 ms', async () => {
   const start = new Date()
   await wait(500)
   const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
+  const delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,17 @@ const getConfig = () => ({
 async function run(): Promise<void> {
   try {
     const config = getConfig()
-    const result = await request({
+    await request({
       url: 'https://api.esoui.com/addons/updatetest',
       method: 'POST',
       headers: { 'x-api-token': config.token },
       formData: {
-        'id': config.addonId,
-        'compatible': config.apiVersions,
-        'updateFile': fs.createReadStream(config.filePath),
-        'version': config.version,
-      }
-    });
+        id: config.addonId,
+        compatible: config.apiVersions,
+        updateFile: fs.createReadStream(config.filePath),
+        version: config.version,
+      },
+    })
 
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
